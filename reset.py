feba791b1d82808cd2755c027d7e03d642859105
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-01-29 13:31:59 -0400

    feat: codigo para resetear archivos en xpr

---
+++ reset.py
@@ -0,0 +1,50 @@
+nombre_archivo = 'direccion'
+
+contenido = [
+    "package src.main;",
+    "",
+    "import com.badlogic.gdx.Input;",
+    "import com.badlogic.gdx.graphics.Color;",
+    "import src.world.entities.player.powers.PowerUp;",
+    "",
+    "public class Modificable {",
+    "    //Variables del juego",
+    "",
+    "    public static final int MONEDAS_POR_MINIJUEGO = 6; // Cantidad maxima de monedas que se pueden obtener en un minijuego",
+    "    public static final int TIEMPO_MINUTOS_JUEGO = 8; // Duración de la partida en minutos",
+    "",
+    "    // Variables del jugador",
+    "",
+    "    public static final int DANO_DASH = 1; // Daño que ocaciona el dash del jugador",
+    "    public static final float FUERZA_IMPULSO_DASH = 15f; // Impulso al hacer el dash",
+    "    public static final float ACELERACION_CAMINAR = 14f;",
+    "    public static final float VELOCIDAD_MAXIMA_CAMINAR = 5f;",
+    "    public static final float ACELERACION_CORRER = 18f;",
+    "    public static final float VELOCIDAD_MAXIMA_CORRER = 6.5f;",
+    "    public static final float TIEMPO_MAXIMO_SALTO_MANTENIDO = 0.3f;",
+    "    public static final float FUERZA_SALTO = 8f;",
+    "    public static final float FUERZA_SALTO_SOSTENIDO = 25f;",
+    "    public static final float FUERZA_ABSORBER = 12f;",
+    "",
+    "    public static final PowerUp.Type PODER_INICIAL = PowerUp.Type.NONE; // Poder inicial del jugador",
+    "    public static final Color COLOR_JUGADOR = new Color(1.0f, 1.0f, 1.0f, 1.0f); // Formato RBG donde:",
+    "    // Todos deben estar entre 0 y 1 (en decimales);",
+    "    // r: rojo | g: verde | b: azul",
+    "    // a: opacidad",
+    "",
+    "    // Controles",
+    "",
+    "    public static int CONTROL_JUGADOR_IZQUIERDA = Input.Keys.A;",
+    "    public static int CONTROL_JUGADOR_DERECHA = Input.Keys.D;",
+    "    public static int CONTROL_JUGADOR_SALTO = Input.Keys.W;",
+    "    public static int CONTROL_JUGADOR_AGACHARSE = Input.Keys.S;",
+    "    public static int CONTROL_JUGADOR_CORRER = Input.Keys.SHIFT_LEFT;",
+    "    public static int CONTROL_JUGADOR_ACCION = Input.Keys.P;",
+    "    public static int CONTROL_JUGADOR_SOLTAR_PODER = Input.Keys.CONTROL_LEFT;",
+    "}"
+]
+
+with open(nombre_archivo, 'w', encoding='utf-8') as archivo:
+    archivo.write('\n'.join(contenido))
+
+print(f'El archivo {nombre_archivo} ha sido sobrescrito con éxito.')
